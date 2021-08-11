be91ee1b5752ebf4c1a18ee0aa742ace4e73a26f
ShinRai <shinrai1030@gmail.com>
2021-08-12 00:10:46 +0530

    Added strongly typed state to user and created initial state const variable

--- apps/apm-demo/src/app/user/+state/user.reducer.ts
+++ apps/apm-demo/src/app/user/+state/user.reducer.ts
@@ -1,10 +1,19 @@
 import { createAction, createReducer, on } from "@ngrx/store";
+import { User } from "../user";
+
+export interface UserState {
+  maskUserName: boolean;
+  currentUser: User;
+}
+
+const initialState: UserState = {
+  maskUserName: true,
+  currentUser: null
+};
 
 export const userReducer = createReducer(
-  {
-    maskUserName: true
-  },
-  on(createAction('[Login] Mask User Name'), state => {
+  initialState,
+  on(createAction('[Login] Mask User Name'), (state): UserState => {
       return {
         ...state,
         maskUserName: !state.maskUserName
